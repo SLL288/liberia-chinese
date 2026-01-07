4d1f3d2e4392017ccc4456e5dfe6166d8ada4a29
SLL28 <65979138+SLL288@users.noreply.github.com>
2026-01-07 14:34:34 +0000

    Switch to static imports in i18n config

--- i18n.ts
+++ i18n.ts
@@ -1,5 +1,7 @@
 import { notFound } from 'next/navigation';
 import { getRequestConfig } from 'next-intl/server';
+import en from '@/content/en';
+import zh from '@/content/zh';
 
 export const locales = ['zh', 'en'] as const;
 export const defaultLocale = 'zh' as const;
@@ -10,11 +12,7 @@ export default getRequestConfig(async ({ locale }) => {
     notFound();
   }
 
-  const messages = locale === 'zh' 
-    ? (await import('@/content/zh')).default
-    : (await import('@/content/en')).default;
-
   return {
-    messages,
+    messages: locale === 'zh' ? zh : en,
   };
 });
